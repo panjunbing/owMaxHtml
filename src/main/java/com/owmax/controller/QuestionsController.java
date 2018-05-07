690d5c4c50d1544740d2057db5b33a40d33846d8
panjunbing <531826568@qq.com>
2018-05-07 18:19:23 +0800

    修改一个bug

--- src/main/java/com/owmax/controller/QuestionsController.java
+++ src/main/java/com/owmax/controller/QuestionsController.java
@@ -58,7 +58,7 @@ public class QuestionsController extends BaseController{
                     map.put("selections",rows);
                 }
                 else {
-                    List<Blanks> blanksList = (List<Blanks>) question.getBlankses();
+                    List<Blanks> blanksList = new ArrayList<>(question.getBlankses());
                     int size = blanksList.size();
                     Map rows[] = new Map[size];
                     for(int i = 0 ;i<size;i++) {
@@ -81,6 +81,66 @@ public class QuestionsController extends BaseController{
         }
         return gson.toJson(map);
     }
+
+    /**
+     * 获取问题
+     * http://localhost:8080/questions/getQuestionByID?id=1
+     * @param id 题号
+     * @return json
+     */
+    @RequestMapping(value = "/getQuestionByID",
+            produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public String getQuestionByID(int id){
+        Map<Object, Object> map = new HashMap<>();
+        try {
+            Questions question = questionsService.get(id);
+            if(question != null) {
+                map.put("title",question.getTitle());
+                int type = question.getType();
+                map.put("type",type);
+                //根据题目类型进行判断
+                //选择题
+                if(type >= 0 && type <= 2 || type == 5){
+                    List<Selections> selectionsList = new ArrayList<>(question.getSelectionses());
+                    int size = selectionsList.size();
+                    Map rows[] = new Map[size];
+                    for(int i = 0 ;i<size;i++) {
+                        Selections selection = selectionsList.get(i);
+                        rows[i] = new HashMap<>();
+                        rows[i].put("selectionID", selection.getId());
+                        rows[i].put("selection", selection.getSelection());
+                        if (type == 2)
+                            rows[i].put("selectionText", selection.getSelectionText());
+                    }
+                    map.put("selections",rows);
+                }
+                else {
+                    List<Blanks> blanksList = new ArrayList<>(question.getBlankses());
+                    int size = blanksList.size();
+                    Map rows[] = new Map[size];
+                    for(int i = 0 ;i<size;i++) {
+                        Blanks blank = blanksList.get(i);
+                        rows[i] = new HashMap<>();
+                        rows[i].put("blank", blank.getBlankText());
+                    }
+                    map.put("blanks",rows);
+                }
+                map.put("result", true);
+            }
+            else {
+                map.put("result", false);
+                map.put("message", "问题ID不存在！");
+            }
+        }
+        catch (Exception e){
+            map.put("result", false);
+            map.put("message",e.getMessage());
+        }
+        return gson.toJson(map);
+    }
+
+
 
     /**
      * 获取问题类型
